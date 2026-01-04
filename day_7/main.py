93d3e377b6c3a8d7e75c749c44672553aa31f83f
SidTheScripter <siddhant.sriraman@gmail.com>
2026-01-04 17:50:27 +0000

    I have reogranized the code in x_y.py and main.py. So now this is the comparing file and not x_y.py

---
+++ day_7/main.py
@@ -0,0 +1,8 @@
+from x_y import compare_x_and_y
+if __name__ == "__main__":
+    x = int(input("Enter value for x: "))
+    y = int(input("Enter value for y: "))
+    answer = compare_x_and_y(x, y)
+    print(answer)
+    print("all done")
+
--- day_7/x_y.py
+++ day_7/x_y.py
@@ -5,9 +5,3 @@ def compare_x_and_y(x, y):
         return "y is greater than x"
     else:
         return "Both values are equal"
-if __name__ == "__main__":
-    x = int(input("Enter value for x: "))
-    y = int(input("Enter value for y: "))
-    answer = compare_x_and_y(x, y)
-    print(answer)
-    print("all done")
